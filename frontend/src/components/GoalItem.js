1294795bf046a2a216262d4897cd4863db23abff
Raju Dhoni <32018260+rajusiripalli@users.noreply.github.com>
2022-04-26 12:51:24 +0530

    added frontend ui

---
+++ frontend/src/components/GoalItem.js
@@ -0,0 +1,16 @@
+
+function GoalItem() {
+  
+
+  return (
+    <div className='goal'>
+      <div></div>
+      <h2></h2>
+      <button  className='close'>
+        X
+      </button>
+    </div>
+  )
+}
+
+export default GoalItem
